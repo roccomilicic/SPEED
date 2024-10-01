cd029d45fd365c707bf70f7e32824b45f8a2d72a
roccomilicic <roccomilicic2@gmail.com>
2024-10-01 21:18:11 +1300

    Set up mongoose connection

--- speed-nest-project/src/app.module.ts
+++ speed-nest-project/src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { ArticlesController } from './api/articles/articles.controller';
 import { ModerationController } from './moderation/moderation.controller';
 import { ModerationService } from './moderation/moderation.service';
-//import { ModerationModule } from './moderation/moderation.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const DB_URI =
+'mongodb+srv://roccomilicic2:ZPFt0VdXL0FnNmRQ@clusterspeed.y8tg4.mongodb.net/?retryWrites=true&w=majority&appName=ClusterSPEED';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(DB_URI)],
   controllers: [AppController, ArticlesController, ModerationController],
   providers: [AppService, ModerationService],
 })
