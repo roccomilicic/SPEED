e852a93acecfea8db426689ac171f16041dbf9d7
Toshiakia <toshiakixu@gmail.com>
2024-10-10 02:01:47 +1300

    create jest setup file

---
+++ speed-nest-project/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    transform: {
+      '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
+    testMatch: ['**/*.spec.ts', '**/*.e2e-spec.ts'],
+    testEnvironmentOptions: {},
+  };
+  
