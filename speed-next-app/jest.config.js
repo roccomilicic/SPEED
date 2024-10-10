7fa53cf8e9b959505d386192fed09010a5f9793b
Toshiakia <toshiakixu@gmail.com>
2024-10-11 00:25:02 +1300

    Update jest.config.js

--- speed-next-app/jest.config.js
+++ speed-next-app/jest.config.js
@@ -2,12 +2,12 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest', // 处理 TypeScript 文件
-    '^.+\\.(js|jsx)$': 'babel-jest', // 处理 JavaScript 和 JSX 文件
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': ['babel-jest', { configFile: './.babelrc.test' }], 
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: [
-    '/node_modules/(?!(@testing-library|react|react-dom)/)', // 忽略 node_modules 中的模块转换，除了指定的库
+    '/node_modules/(?!(react|react-dom|@testing-library)/)',  
   ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
