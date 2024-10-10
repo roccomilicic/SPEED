a09de4f019f86a0b246d15c6ae472aa5d1b1b04b
Toshiakia <toshiakixu@gmail.com>
2024-10-11 01:06:15 +1300

    fix

--- speed-nest-project/jest.config.js
+++ speed-nest-project/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom', 
+  testEnvironment: 'node', 
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
--- speed-next-app/jest.config.js
+++ speed-next-app/jest.config.js
@@ -3,11 +3,14 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
-    '^.+\\.(js|jsx)$': ['babel-jest', { configFile: './.babelrc.test' }], 
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { configFile: './.babelrc.test' }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: [
-    '/node_modules/(?!(react|react-dom|@testing-library)/)',  
+    'node_modules/(?!(react|react-dom|@testing-library)/)',
   ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1'
+  },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
