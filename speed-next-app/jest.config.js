8354bf97124cd814787ec5cfa8b7ea8ab2705c41
Toshiakia <toshiakixu@gmail.com>
2024-10-11 00:17:20 +1300

    temp not test "test/article.e2e-spec.ts" "test/app.e2e-spec.ts" "src/app.controller.spec.ts"

--- speed-next-app/jest.config.js
+++ speed-next-app/jest.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jest-environment-jsdom',
-    transform: {
-      '^.+\\.(ts|tsx)$': 'ts-jest',  
-      '^.+\\.(js|jsx)$': 'babel-jest',  
-    },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    transformIgnorePatterns: ['/node_modules/'],
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest', // 处理 TypeScript 文件
+    '^.+\\.(js|jsx)$': 'babel-jest', // 处理 JavaScript 和 JSX 文件
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@testing-library|react|react-dom)/)', // 忽略 node_modules 中的模块转换，除了指定的库
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
