ae5e0c7ebf4be30e3cee25bb7f9dbf79ae3f0f07
roccomilicic <roccomilicic2@gmail.com>
2024-10-11 14:58:51 +1300

    refactor: integrate Express with NestJS and update Vercel configuration

--- speed-nest-project/src/main.ts
+++ speed-nest-project/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express'; // Import the ExpressAdapter
+import * as express from 'express'; // Import Express
+
+const app = express(); // Create an instance of Express
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule); // enable cors
-  app.enableCors({ origin: true, credentials: true });
-  const port = process.env.PORT || 8082;
-  await app.listen(port, () => console.log(`Server running on port ${port}`));
+  const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(app)); // Use the ExpressAdapter
+  nestApp.enableCors({ origin: true, credentials: true }); // Enable CORS
+  await nestApp.init(); // Initialize the Nest application
 }
+
+// Start the Nest application
 bootstrap();
+
+// Export the app for Vercel serverless functions
+export default app; // This line allows Vercel to access the app
