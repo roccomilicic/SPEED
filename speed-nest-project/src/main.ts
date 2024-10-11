5e3e566a9418bfbba3b3d82eddf8cbb8f58f1256
roccomilicic <roccomilicic2@gmail.com>
2024-10-11 15:34:11 +1300

    chore: update Vercel configuration and set Node.js engine requirement

--- speed-nest-project/src/main.ts
+++ speed-nest-project/src/main.ts
@@ -1,18 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express'; // Import the ExpressAdapter
-import * as express from 'express'; // Import Express
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
-const app = express(); // Create an instance of Express
+const server = express(); // Create an instance of Express
 
 async function bootstrap() {
-  const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(app)); // Use the ExpressAdapter
-  nestApp.enableCors({ origin: true, credentials: true }); // Enable CORS
-  await nestApp.init(); // Initialize the Nest application
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server)); // Use ExpressAdapter
+  app.enableCors({ origin: true, credentials: true }); // Enable CORS
+  await app.init(); // Initialize the Nest application
 }
 
-// Start the Nest application
 bootstrap();
 
-// Export the app for Vercel serverless functions
-export default app; // This line allows Vercel to access the app
+// Export the Express server as the default export for Vercel
+export default server;
