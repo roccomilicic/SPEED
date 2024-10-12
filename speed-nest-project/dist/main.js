72086e81d2a4eec83cba19f62c5b5e2a1f80d734
roccomilicic <roccomilicic2@gmail.com>
2024-10-12 21:46:58 +1300

    chore: update backend URL and add GitHub Actions workflows for testing and deployment

--- speed-nest-project/dist/main.js
+++ speed-nest-project/dist/main.js
@@ -2,11 +2,20 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const mongoose_1 = require("mongoose");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    try {
+        const connection = await (0, mongoose_1.connect)("mongodb+srv://rocco:mcY9ItydBZONj0Nu@clusterspeed.y8tg4.mongodb.net/?retryWrites=true&w=majority&appName=ClusterSPEED");
+        console.log('Connected to MongoDB:', connection.connection.name);
+    }
+    catch (error) {
+        console.error('Error connecting to MongoDB:', error.message);
+    }
     app.enableCors({ origin: true, credentials: true });
     const port = process.env.PORT || 8082;
-    await app.listen(port, () => console.log(`Server running on port ${port}`));
+    await app.listen(port);
+    console.log('Nest application is running on: http://localhost:8082');
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- speed-nest-project/src/main.ts
+++ speed-nest-project/src/main.ts
@@ -1,11 +1,40 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors({
+//     origin: 'https://speed-vercel-ehmmzquy9-roccos-projects-5f59ad15.vercel.app', 
+//     methods: 'GET,POST,PUT,DELETE', 
+//     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+//     credentials: true, 
+//   });
+//   const port = process.env.PORT || 8082;
+//   await app.listen(port, () => console.log(`Server running on port ${port}`));
+// }
+
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { connect } from 'mongoose'; // Import connect directly
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule); // enable cors
+  const app = await NestFactory.create(AppModule);
+
+  // Test MongoDB Connection
+  try {
+    const connection = await connect("mongodb+srv://rocco:mcY9ItydBZONj0Nu@clusterspeed.y8tg4.mongodb.net/?retryWrites=true&w=majority&appName=ClusterSPEED"); // Use the connect method directly
+    console.log('Connected to MongoDB:', connection.connection.name); // Log the name of the database
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message); // Log connection error
+  }
+
   app.enableCors({ origin: true, credentials: true });
   const port = process.env.PORT || 8082;
-  await app.listen(port, () => console.log(`Server running on port ${port}`));
+
+  await app.listen(port);
+  console.log('Nest application is running on: http://localhost:8082');
 }
 
 bootstrap();
